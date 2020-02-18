c5c970a1dd41d1a18ebd3ce407dfb7acbeea4568
JordyLoos <jordyisloosvelt@gmail.com>
2020-02-18 11:08:42 +0100

    Create aanmelden.js

---
+++ aanmelden.js
@@ -0,0 +1,33 @@
+"use strict";
+
+document.getElementById("Registreren").onclick = new function() {
+    const verkeerdeElementen =
+        document.querySelectorAll("input:invalid,select:invalid");
+    for (const element of verkeerdeElementen) {
+        document.getElementById(`${element.id}Fout`).style.display = "inline";
+    }
+    const correcteElementen =
+        document.querySelectorAll("input:valid,select:valid");
+    for (const element of correcteElementen) {
+        document.getElementById(`${element.id}Fout`).style.display = "";
+    }
+
+    if (invoerCorrect) {
+        toevoegen()
+
+    }
+}
+
+function invoerCorrect() {
+    const verkeerdeElementen =
+        document.querySelectorAll("input:invalid,select:invalid");
+    for (const element of verkeerdeElementen) {
+        document.getElementById(`${element.id}Fout`).style.display = "inline";
+    }
+    const correcteElementen =
+        document.querySelectorAll("input:valid,select:valid");
+    for (const element of correcteElementen) {
+        document.getElementById(`${element.id}Fout`).style.display = "";
+    }
+    return verkeerdeElementen.length === 0;
+}
