81c6c8870b08b1bfd0972fc8e621ee8c5258cf5a
ZegMaarJoe <willaertjonathan@gmail.com>
2020-02-18 11:43:38 +0100

    profielen afhalen
    stukje code om alle profielen te fetchen

--- zoeken.js
+++ zoeken.js
@@ -1,1 +1,15 @@
-khkjhkjjk
+"use strict";
+
+profielenAfhalen();
+async function profielenAfhalen() {
+    const response = await fetch("https://scrumserver.tenobe.org/scrum/api/profiel/read.php");
+    if (response.ok) {
+        const profielen = await response.json();
+        console.log(profielen);
+    } else {
+        document.getElementById("nietGevonden").style.display = "block";
+    }
+}
+
+
+
