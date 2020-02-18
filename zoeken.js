8f3ee219d7ca362b3c43f194f0b98952ad86e91c
ZegMaarJoe <willaertjonathan@gmail.com>
2020-02-18 15:40:22 +0100

    zoekfunctie begin code
    voorlopige versie van de zoekfunctie

--- zoeken.js
+++ zoeken.js
@@ -9,7 +9,56 @@ async function profielenAfhalen() {
     } else {
         document.getElementById("nietGevonden").style.display = "block";
     }
+};
+
+/*
+profielenZoeken();
+async function profielenZoeken() {
+    const response = await fetch("https://scrumserver.tenobe.org/scrum/api/profiel/search/.php");
+    if (response.ok) {
+        const profielen = await response.json();
+        console.log(profielen);
+    } else {
+        document.getElementById("nietGevonden").style.display = "block";
+    }
 }
 
 
+eenProfielAfhalen();
+async function eenProfielAfhalen() {
+    const response = await fetch("https://scrumserver.tenobe.org/scrum/api/profiel/read_one?profielId=2.php");
+    if (response.ok) {
+        const eenProfiel = await response.json();
+        console.log(eenProfiel);
+    } else {
+        document.getElementById("nietGevonden").style.display = "block";
+    }
+};
 
+leesGegevens1();
+        
+            
+        async function leesGegevens1()
+        {
+            const zoekProfiel = await fetch( 'https://scrumserver.tenobe.org/scrum/api/profiel/search.php?sexe="m"' );
+           // https://scrumserver.tenobe.org/scrum/api
+            const gezochtProfiel = await fetch(zoekProfiel); 
+            console.log(gezochtProfiel);
+        };
+        
+      //  let url=rooturl+'/profiel/search.php?geboortedatum='+ geboortedatum + '&geboortedatumOperator='+ geboortedatumOperator;
+
+
+*/
+      async function()
+      {
+        let geslacht  =  document.getElementById('sexe').value;
+        const rooturl = 'https://scrumserver.tenobe.org/scrum/api';
+        let url =rooturl+'/profiel/search.php?sexe='+geslacht;
+        
+        fetch(url)
+            .then(function (resp)   { return resp.json(); })
+            .then(function (data)   { console.log(data);  })
+            .catch(function (error) { console.log(error); })
+      };
+      zoekOpGeslacht();
