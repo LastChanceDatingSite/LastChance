511dd402731292b1ea4f1864db19ee3e161177c7
ZegMaarJoe <willaertjonathan@gmail.com>
2020-02-19 15:01:48 +0100

    aanmelden werkt
    aanmelden leidt naar gebruikersPagina

---
+++ aanmelden.js
@@ -0,0 +1,41 @@
+"use strict";
+
+    
+    //const gebruikerWachtwoord = document.getElementById("wachtwoord").value;
+
+    profielenAfhalen();
+    async function profielenAfhalen() 
+    {
+        const response = await fetch("https://scrumserver.tenobe.org/scrum/api/profiel/read.php");
+        if (response.ok) 
+        {
+            const profielen = await response.json();
+            console.log(profielen);
+            document.getElementById("aanmelden").onclick = function()
+            {
+                const gebruikerNickname = document.getElementById("user").value;
+                console.log(gebruikerNickname);
+                for (const profiel of profielen)
+                {
+                    console.log(profiel.nickname);
+                    if (gebruikerNickname === profiel.nickname)
+                    {
+                        console.log("juist");
+                        window.location.replace("gebruikers.html");
+                    }
+                    else
+                    {
+                        console.log("mis");
+                    }
+                }
+            }
+
+        } 
+        else 
+        {
+            document.getElementById("aanmeldingFout").style.display = "block";
+        };
+    };
+
+
+
