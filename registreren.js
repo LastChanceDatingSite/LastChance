63fc542449648309bb014e19bfb00668ff0fb0dc
JordyLoos <61182432+JordyLoos@users.noreply.github.com>
2020-02-19 13:47:47 +0100

    FOUTEN LUKKEN

--- registreren.js
+++ registreren.js
@@ -42,9 +42,36 @@ async function persoonToevoegen() {
         lovecoins: "3"
     };
 
-    console.log(data.lovecoins);
+    /* let quotes=["Chuck Norris can divide by zero.",
+    "Chuck Norris can kill two stones with one bird.",
+    "Outer space exists because it's afraid to be on the same planet with Chuck Norris.",
+    "Chuck Norris counted to infinity ... three times.",
+    "Chuck Norris can slam a revolving door.",
+    "There is no theory of evolution. Just a list of creatures Chuck Norris has allowed to live.",
+    "Chuck Norris is so fast, he can run around the world and punch himself in the back of the head.",
+    "If you have five dollars and Chuck Norris has five dollars, Chuck Norris has more money than you."];
 
-   
+    let min = 0;
+    let max = quotes.length-1;
+    let rndIndex = Math.floor(Math.random() * (max - min + 1) + min);
+
+    let data = {
+        familienaam: "Norris",
+        voornaam: "Chuck" ,
+        geboortedatum: "0001-01-01",
+        email: "me@Chuck.Norris",
+        nickname: "The Chuck" + Date.now(),
+        foto: "no_picture.jpg",
+        beroep: "Moviestar",
+        sexe: "x",
+        haarkleur: "brown",
+        oogkleur: "blue",
+        grootte: "1095",
+        gewicht: "",
+        wachtwoord: "iamgod",
+        metadata: "",
+        lovecoins: "1000000"
+    } */
 
     var request = new Request(url, {
         method: 'POST',
