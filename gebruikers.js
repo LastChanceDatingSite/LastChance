66b501ff16e993965a85d7ddc6fb923a703b8f63
ZegMaarJoe <willaertjonathan@gmail.com>
2020-02-21 14:42:01 +0100

    weerhier

--- gebruikers.js
+++ gebruikers.js
@@ -12,7 +12,7 @@ async function eenProfielAfhalen() {
         console.log(eenProfiel);
         document.getElementById("begroeting").innerText = "Welkom " + eenProfiel.nickname;
         lijstGebruikers(eenProfiel);
-        sterrenbeeldAfhalen(eenProfiel);
+        document.getElementById("sterrenbeeld").src = sterrenbeeldAfhalen(eenProfiel);
     }
     else {
         document.getElementById("nietGevonden").style.display = "block";
@@ -38,6 +38,42 @@ function lijstGebruikers(gebruiker) {
 
 function sterrenbeeldAfhalen(gebruiker)
 {
-    
+        //let sterrenbeeldAfbeelding = document.getElementById("sterrenbeeld").src;
+        console.log(gebruiker.geboortedatum);
+        const geboortedatum = new Date(gebruiker.geboortedatum);
+        const month = geboortedatum.getMonth() + 1;
+        console.log(month);
+        const day = geboortedatum.getDay();
+        console.log(day);
+      
+        if((month == 1 && day <= 20) || (month == 12 && day >=22)) {
+            return "img/sterrenbeeld/steenbok.png";
+        } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
+          return "img/sterrenbeeld/waterman.png";
+        } else if((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+          return "img/sterrenbeeld/vissen.png";
+        } else if((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+          return "img/sterrenbeeld/ram.png";
+        } else if((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+          return "img/sterrenbeeld/stier.png";
+        } else if((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+          return "img/sterrenbeeld/tweelingen.png";
+        } else if((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
+          return "img/sterrenbeeld/kreeft.png";
+        } else if((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
+          return "img/sterrenbeeld/leeuw.png";
+        } else if((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
+          return "img/sterrenbeeld/maagd.png";
+        } else if((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
+          return "img/sterrenbeeld/weegschaal.png";
+        } else if((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
+          return "img/sterrenbeeld/schorpioen.png";
+        } else if((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+          return "img/sterrenbeeld/boogschutter.png";
+        }
+
+
+        
+        
 }
 
