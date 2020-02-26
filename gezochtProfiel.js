46e5c4096df48810adb33e9b573adcbf9b1e1594
wzrdOfRavenPuff <56390323+wzrdOfRavenPuff@users.noreply.github.com>
2020-02-26 23:12:47 +0100

    topmenu berichten toegevoegd
    en in gezocht profiel  (html+js) button gemaakt om bericht te sturen naar gebruiker

--- gezochtProfiel.js
+++ gezochtProfiel.js
@@ -186,6 +186,11 @@ async function favorietcontrole() {
 }
 
 
-
+//stuur een bericht naar deze persoon:
+document.getElementById("stuurEenBericht").addEventListener("click", function (e) {
+    const berichtNaarDezeGebruiker = localStorage.getItem("gezochteGebruiker");
+    localStorage.setItem("berichtenAndereGebruiker",berichtNaarDezeGebruiker); 
+    window.location.replace("chatbox.html");
+});
 
   
